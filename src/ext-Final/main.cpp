8099a6cf4c3a507ac5a5086f7bdf9308d97c572c
HHHUUUGGGOOO <hugo551565@gmail.com>
2021-12-26 11:53:35 +0800

    basic framwork definition

--- src/ext-Final/main.cpp
+++ src/ext-Final/main.cpp
@@ -6,12 +6,79 @@
   Copyright    [ Copyleft(c), LSV, NTUEE, Taiwan ]
 ****************************************************************************/
 
+// package
 #include <iostream>
 #include <algorithm>
 #include <string>
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
-
+// self-file
+#include "thresDef.h"
+// abc
 
 using namespace std;
+
+//----------------------------------------------------------------------
+//    initialize from ABC 
+//----------------------------------------------------------------------
+extern "C"
+{
+    // Abc_Ntk_t_ --> Aig_Man_t (src/base/abci/abcDar.c)
+    Aig_Man_t *  Abc_NtkToDar( Abc_Ntk_t * pNtk, int fExors, int fRegisters );
+
+    // procedures to start and stop the ABC framework
+    // (should be called before and after the ABC procedures are called)
+    void   Abc_Start();
+    void   Abc_Stop();
+
+    // procedures to get the ABC framework and execute commands in it
+    typedef struct Abc_Frame_t_ Abc_Frame_t;
+    Abc_Frame_t * Abc_FrameGetGlobalFrame();
+
+    // used in "sprintf (refer to template in Google drive)" --> execute "command in abc"
+    // work like ".script" --> automatically execute all operation
+    int    Cmd_CommandExecute( Abc_Frame_t * pAbc, const char * sCommand );
+
+}
+
+Abc_Frame_t * pAbc = 0;
+
+//----------------------------------------------------------------------
+//    add new command
+//----------------------------------------------------------------------
+void init(Abc_Frame_t* pAbc)
+{
+  Cmd_CommandAdd(pAbc, "LSV", "collapse", LSV_CommandCollapse, 0);
+  Cmd_CommandAdd(pAbc, "LSV", "unate_check", LSV_CommandUnateCheck, 0);
+  Cmd_CommandAdd(pAbc, "LSV", "ILP_check", LSV_CommandILPCheck, 0);
+  Cmd_CommandAdd(pAbc, "LSV", "unate_split", LSV_CommandUnateSplit, 0);
+  Cmd_CommandAdd(pAbc, "LSV", "binate_split", LSV_CommandBinateSplit, 0);
+}
+
+void destroy(Abc_Frame_t* pAbc) {}
+
+Abc_FrameInitializer_t frame_initializer = { init, destroy };
+
+struct PackageRegistrationManager
+{
+  PackageRegistrationManager() { Abc_FrameAddInitializer(&frame_initializer); }
+} lsvPackageRegistrationManager;
+
+
+//----------------------------------------------------------------------
+//    main function
+//----------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+  // setup
+  Abc_Start();
+
+
+  // terminate
+  Abc_Stop();
+  return 0;
+
+}
+
+
--- src/ext-Final/thresDef.h
+++ src/ext-Final/thresDef.h
@@ -15,11 +15,191 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+// abc
+#include "base/abc/abc.h"
+#include "base/main/main.h"
+#include "base/main/mainInt.h"
+#include "misc/util/abc_global.h"
 
 
 using namespace std;
 
+//----------------------------------------------------------------------
+//    each algorithm;s function
+//----------------------------------------------------------------------
+void LSV_Collapse(Abc_Ntk_t* pNtk);
+bool LSV_UnateCheck(Abc_Ntk_t* pNtk);
+bool LSV_ILPCheck(Abc_Ntk_t* pNtk);
+void LSV_UnateSplit(Abc_Ntk_t* pNtk);
+void LSV_BinateSplit(Abc_Ntk_t* pNtk);
 
+//----------------------------------------------------------------------
+//    add new command
+//----------------------------------------------------------------------
+// collapse
+static int LSV_CommandCollapse(Abc_Frame_t* pAbc, int argc, char** argv)
+{
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  int c;
+  Extra_UtilGetoptReset();
+  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF)
+  {
+    switch (c)
+    {
+      case 'h':
+        goto usage;
+      default:
+        goto usage;
+    }
+  }
+  if (!pNtk)
+  {
+    Abc_Print(-1, "Empty network.\n");
+    return -1;
+  }
+  // main function
+  LSV_Collapse(pNtk);
+  return 0;
+
+usage:
+  Abc_Print(-2, "usage: collapse [-h]\n");
+  Abc_Print(-2, "\t        collapse node\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+
+}
+
+// unate checking
+static int LSV_CommandUnateCheck(Abc_Frame_t* pAbc, int argc, char** argv)
+{
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  int c;
+  Extra_UtilGetoptReset();
+  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF)
+  {
+    switch (c)
+    {
+      case 'h':
+        goto usage;
+      default:
+        goto usage;
+    }
+  }
+  if (!pNtk)
+  {
+    Abc_Print(-1, "Empty network.\n");
+    return -1;
+  }
+  // main function
+  LSV_UnateCheck(pNtk);
+  return 0;
+
+usage:
+  Abc_Print(-2, "usage: unate_check [-h]\n");
+  Abc_Print(-2, "\t        check if the input network is unate\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+
+}
+
+// ILP checking if threshold
+static int LSV_CommandILPCheck(Abc_Frame_t* pAbc, int argc, char** argv)
+{
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  int c;
+  Extra_UtilGetoptReset();
+  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF)
+  {
+    switch (c)
+    {
+      case 'h':
+        goto usage;
+      default:
+        goto usage;
+    }
+  }
+  if (!pNtk)
+  {
+    Abc_Print(-1, "Empty network.\n");
+    return -1;
+  }
+  // main function
+  LSV_ILPCheck(pNtk);
+  return 0;
+
+usage:
+  Abc_Print(-2, "usage: ILP_check [-h]\n");
+  Abc_Print(-2, "\t        check if the network is threshold\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+
+}
+
+// unate splitting
+static int LSV_CommandUnateSplit(Abc_Frame_t* pAbc, int argc, char** argv)
+{
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  int c;
+  Extra_UtilGetoptReset();
+  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF)
+  {
+    switch (c)
+    {
+      case 'h':
+        goto usage;
+      default:
+        goto usage;
+    }
+  }
+  if (!pNtk)
+  {
+    Abc_Print(-1, "Empty network.\n");
+    return -1;
+  }
+  // main function
+  LSV_UnateSplit(pNtk);
+  return 0;
+
+usage:
+  Abc_Print(-2, "usage: unate_split [-h]\n");
+  Abc_Print(-2, "\t        do unate splitting\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+
+}
+
+// binate splitting
+static int LSV_CommandBinateSplit(Abc_Frame_t* pAbc, int argc, char** argv)
+{
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  int c;
+  Extra_UtilGetoptReset();
+  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF)
+  {
+    switch (c)
+    {
+      case 'h':
+        goto usage;
+      default:
+        goto usage;
+    }
+  }
+  if (!pNtk)
+  {
+    Abc_Print(-1, "Empty network.\n");
+    return -1;
+  }
+  // main function
+  LSV_BinateSplit(pNtk);
+  return 0;
+
+usage:
+  Abc_Print(-2, "usage: binate_split [-h]\n");
+  Abc_Print(-2, "\t        do binate splitting\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+
+}
 
 
 #endif // _THRESDEF_H
